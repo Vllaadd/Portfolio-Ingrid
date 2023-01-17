c20cd251cef9360f21e6bd18966b3547e24decc3
Vllaadd <vlladdiimiir@gmail.com>
2023-01-17 05:34:30 -0500

    get method

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("index route is working")
+})
+
 const port = process.env.PORT || 8080;
 app.listen(port);
 console.log("Express started listenining on port ", port)
