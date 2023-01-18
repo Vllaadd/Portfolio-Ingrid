f5497df12c6b897bc9ccbfec5b2fd50333c8de21
Vllaadd <vlladdiimiir@gmail.com>
2023-01-18 01:50:53 -0500

    handlebars setup

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
+const handlebars = require("express-handlebars") 
 const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"))
+app.engine(".hbs", handlebars({
+    extname: ".hbs",
+    defaultLayout: false
+}));
+app.set("view engine", ".hbs");
+
 
 app.get("/", function(req, res){
     res.send("index route is working")
